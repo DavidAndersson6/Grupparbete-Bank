1e26d46536de1a1fee31b80f2c4a0e2f095085c4
admin <admin@DESKTOP-KVNK9SN>
2024-11-01 16:54:48 +0100

    Add project files.

---
+++ Grupparbete Bank/Transactions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Grupparbete_Bank
+{
+    public class Transactions
+    {
+        List<Transaction> transactions = new List<Transaction>();
+
+    }
+}
