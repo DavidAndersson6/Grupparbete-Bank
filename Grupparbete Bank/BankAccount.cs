8750068ded0a644e982576222e2a78d30902cd45
DavidAndersson6 <davidsvenandersson@gmail.com>
2024-11-04 16:42:08 +0100

    Added function ListAccounts

---
+++ Grupparbete Bank/BankAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Grupparbete_Bank
+{
+    public class BankAccount
+    {
+        public int AccountId { get; set; }
+        public decimal Balance { get; set; }
+        public string AccountHolder { get; set; }
+
+        public BankAccount(int accountId, decimal balance, string accountHolder)
+        {
+            AccountId = accountId;
+            Balance = balance;
+            AccountHolder = accountHolder;
+        }
+
+
+        
+
+
+
+
+
+        public void ListAccounts()
+        {
+            List<BankAccount> bankAccounts = new List<BankAccount> ();
+            bankAccounts.Add(new BankAccount(1, 1000.5m, "Alice"));
+            bankAccounts.Add(new BankAccount(2, 600.755m, "Markus"));
+
+
+
+            foreach (BankAccount bankAccount in bankAccounts)
+            {
+                Console.WriteLine($"Account ID: {bankAccount.AccountId}, Balance: {bankAccount.Balance}, Holder: {bankAccount.AccountHolder}");
+            }
+        }
+    }
+}
